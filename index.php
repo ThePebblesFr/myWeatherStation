9f76543f9e67167a365812f44f6b20f92ab9243f
Pierre <pierre.garreau@etu.emse.fr>
2022-09-16 19:12:51 +0200

    Menu started

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+/*
+    __________________________________________________________________________
+   |                                                                          |
+   |                      MY WHEATHER STATION - INDEX                         |
+   |                                                                          |
+   |    Author            :   P. GARREAU, M. JALES                            |
+   |    Status            :   Under Development                               |
+   |    Last Modification :   16/09/2022                                      |
+   |    Project           :   EMBEDDED LINUX PROJECT                          |
+   |                                                                          |
+   |__________________________________________________________________________|
+
+*/
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,6 +22,31 @@
         <script type="text/javascript" src="js/jQuery.js"></script>
     </head>
     <body>
-    <script type="text/javascript" src="js/dimensions.js"></script>
+        <section class="leftContainer">
+            <section class="menuContainer">
+                <div class="menuItemContainer">
+                    <img src="assets/images/home_icon.png" class="menuIcon"/>
+                </div>
+                <div class="menuItemContainer">
+                    <img src="assets/images/temperature_icon.png" class="menuIcon"/>
+                </div>
+                <div class="menuItemContainer">
+                    <img src="assets/images/humidity_icon.png" class="menuIcon"/>
+                </div>
+                <div class="menuItemContainer">
+                    <img src="assets/images/pressure_icon.png" class="menuIcon"/>
+                </div>
+            </section>
+            <section class="logoMinesContainer">
+                <img src="assets/images/logo_mines.png" class="logoMines"/>
+            </section>
+        </section>
+        <section class="bodyContainer">
+
+        </section>
+        <script type="text/javascript">
+            
+        </script>
+        <script type="text/javascript" src="js/dimensions.js"></script>
     </body>
 </html>
